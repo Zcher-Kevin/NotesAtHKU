b4900cfa2660a3b605dc1dc81310f4c4fff38124
Jax Tam <80020581+EnhancedJax@users.noreply.github.com>
2025-04-25 20:45:42 +0800

    fix build issues with mermaid

--- source.config.ts
+++ source.config.ts
@@ -1,6 +1,6 @@
+import { remarkMermaid } from "@theguild/remark-mermaid";
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
 import rehypeKatex from "rehype-katex";
-import rehypeMermaid from "rehype-mermaid";
 import remarkMath from "remark-math";
 import { remarkAdmonition } from "./remarkPlugins/admonition";
 
@@ -10,11 +10,10 @@ export const { docs, meta } = defineDocs({
 
 export default defineConfig({
   mdxOptions: {
-    remarkPlugins: [remarkAdmonition, remarkMath],
+    remarkPlugins: [remarkAdmonition, remarkMath, remarkMermaid],
     // Place it at first so that it won't be changed by syntax highlighter
     rehypePlugins: (v) => [
       [rehypeKatex, { trust: true, strict: "ignore" }],
-      rehypeMermaid,
       ...v,
     ],
   },
